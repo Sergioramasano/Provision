05510e10dcec63cab6d9106d5d626a428d19aa7f
Sergioramasano <razumenko.serg@gmail.com>
2019-06-13 16:50:46 +0300

    THE END

--- JS/navbar.js
+++ JS/navbar.js
@@ -6,43 +6,22 @@ $(() => {
     const show = () => {
         $left_menu.attr('aria-hidden', false);
         $layer.attr('aria-hidden', false);
-        $left_menu.css({left: '-400%'}).animate({left: 0}, animTime);
-        layerAnimate(true, animTime)
-        //$layer.css({opacity: 1}).fadeIn(animTime);
+        $left_menu.css({left: '-400%'}).animate({left: 0}, {queue: false, duration: animTime});
+        $layer.css({display: 'block'}).animate({backgroundColor: 'rgba(0,0,0,0.8)'}, {queue: false, duration: animTime});
+        $layer.css({opacity: 1}).fadeIn(animTime);
     }
 
     const hide = () => {
         $left_menu.attr('aria-hidden', true);
         $layer.attr('aria-hidden', true);
-        $left_menu.animate({left: '-100%'}, animTime);
-        layerAnimate(false, animTime);
-        //$layer.fadeOut(animTime);
+        $left_menu.animate({left: '-100%'}, {queue: false, duration: animTime});
+        $layer.animate({
+            backgroundColor: 'rgba(0,0,0,0)'
+        }, {queue: false, duration: animTime});
+        $layer.fadeOut(animTime);
     }
 
     $(".navbar-toggler").click(show);
     $(".closer").click(hide);
     $layer.click(hide);
 });
-
-const layerAnimate = (dir = true, duration = 5000) => {
-    const layer = document.getElementsByClassName('layer')[0];
-
-    const action = k => {
-        layer.style.backgroundColor = `rgba(0,0,0,${k * 0.8})`;
-
-        layer.style.display = !k ? 'none' : 'block'
-    }
-
-    const savetime = Date.now();
-    const timer = setInterval(() => {
-        const delta = Date.now() - savetime;
-        if (dir)
-            action(delta / duration);
-        else
-            action(1 - (delta / duration));
-        if (delta >= duration) {
-            clearInterval(timer);
-            action(~~dir);
-        }
-    }, 10);
-}
